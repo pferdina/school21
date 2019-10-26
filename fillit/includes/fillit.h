2125c4c52122e64f8630d5b79a29fb406604a0d3
Andrew Lvov <pferdina>
2019-10-26 20:36:45 +0300

    leha lovi bibu

--- fillit/includes/fillit.h
+++ fillit/includes/fillit.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fillit.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pferdina <pferdina@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pferdina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 17:35:59 by pferdina          #+#    #+#             */
-/*   Updated: 2019/10/23 19:02:03 by pferdina         ###   ########.fr       */
+/*   Updated: 2019/10/26 20:36:00 by pferdina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,19 @@ typedef struct  s_point
     int         y;
 }               point;
 
+typedef struct  s_tetris
+{
+        char    name;
+        point   *next;
+        point   *tetri;
+}               tetris;
+
+
 void    read_data(char *file_name);
 int     check_data(char *data);
 int     count_sharps(char *data);
 int     check_symbols(char *data);
 int     check_tetri(char *data);
+int     save_tetri(point *sharps);
 
 # endif
--- fillit/srcs/check.c
+++ fillit/srcs/check.c
@@ -6,7 +6,7 @@
 /*   By: pferdina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 16:53:02 by pferdina          #+#    #+#             */
-/*   Updated: 2019/10/24 11:59:43 by pferdina         ###   ########.fr       */
+/*   Updated: 2019/10/25 19:37:41 by pferdina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ point   *get_sharps(char *data)
         }
         i++;
     }
-    return (&sharps[0]);
+    return (sharps);
 }
 
 int    check_tetri(char *data)
@@ -85,19 +85,24 @@ int    check_tetri(char *data)
     sharps = get_sharps(data);
     while (i < 4)
     {
-        if (sharps[i].x + 1 == sharps[i + 1].x || 
-            sharps[i].y + 1 == sharps[i + 1].y)
+        if ((sharps[i].x + 1 == sharps[i + 1].x && sharps[i].y == sharps[i + 1].y) || 
+            (sharps[i].y + 1 == sharps[i + 1].y && sharps[i].x == sharps[i + 1].x))
             counter += 2;
         if (i == 0 && sharps[i].x + 1 == sharps [i + 3].x &&
             sharps[i].y + 1 == sharps [i + 3].y &&
             sharps[i + 1].x - 1 == sharps[i + 2].x && 
             sharps[i + 1].y + 1 == sharps[i + 2].y)
             counter += 2;
+        if ((i == 0 || i == 2) && sharps[i].x - 1 == sharps[i + 1].x &&
+            sharps[i].y + 1 == sharps[i + 1].y)
+            counter += 2;
         i++;
     }
     printf("%d\n", counter);
     if (counter != 6  && counter != 8)
         return (0);
+/*    if (!save_tetri(sharps))
+        return (0); */
     return (1);
 }
 
